dec33f76343f1e0184ee55a7561a42ac6fa67a62
Vic7ord1st <okoroafor.victor@gmail.com>
2019-08-29 03:24:42 +0100

    documented the code

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+// The getValue function to receive all click events
 function getValue(value) {
-    console.log(value);
+    //Checking for systax error in the calculator display
     if (document.querySelector('.display').textContent == 'syntax error') {
         document.querySelector('.display').style.color = "rgb(0, 0, 0)";
+
         if (value = 'clear') {
             document.querySelector('.display').textContent = '0'
         }
@@ -9,21 +11,24 @@ function getValue(value) {
             document.querySelector('.display').textContent = value;
         }
     }
+
     else {
+        //if the button pushed is the clear button call the clear function
         if (value == 'clear') {
-            console.log('it is clear!')
             clearDisplay();
         }
+        //if the button clicked is the equals button call the calculate function
         else if (value == 'equals') {
-            console.log('equals');
             let query = document.querySelector('.display').textContent;
             calculate(query);
         }
+        //if sqrt is clicked call the squareroot function on the value from the display and pass the result to calculate
         else if (value == 'sqrt') {
             let sqrtQuery = document.querySelector('.display').textContent;
             let query = squareroot(sqrtQuery);
             calculate(query);
         }
+        //if the decimal is clicked add it to the existing items on the display
         else if (value == '.') {
             let query = document.querySelector('.display').textContent + value;
             document.querySelector('.display').textContent = query;
@@ -39,12 +44,13 @@ function getValue(value) {
     }
 };
 
+//the clearDisplay function
 function clearDisplay() {
     document.querySelector('.display').textContent = '0';
 };
 
+//the calculate function
 function calculate(value) {
-    console.log(value);
 
     let result;
     try {
@@ -57,6 +63,7 @@ function calculate(value) {
     document.querySelector('.display').textContent = result.toFixed(2);
 }
 
+//the squareroot function
 function squareroot(value) {
     return `Math.sqrt(${value})`
 }
